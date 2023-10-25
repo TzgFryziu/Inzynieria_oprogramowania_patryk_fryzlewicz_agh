bb930eb885d0b51c024c3d8254f5188acf5d6d60
TzgFryziu <85579231+TzgFryziu@users.noreply.github.com>
2023-10-25 16:17:26 +0200

    Add files via upload

---
+++ Zadanie 4/Zadanie 4/Zadanie 4.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+class Produkt {
+public:
+    string nazwa{};
+    float cena{};
+    Produkt(string nazwa_i, float cena_i)
+        :nazwa{ nazwa_i }, cena{ cena_i } {};
+    Produkt()
+        :Produkt("", 0) {};
+};
+
+void bubbleSort(Produkt arr[], int n)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j].cena < arr[j + 1].cena) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (swapped == false)
+            break;
+    }
+}
+
+int main()
+{
+    
+    Produkt tab[10];
+    for (int i = 0; i < 10; i++) {
+        float cena{};
+        string nazwa{};
+        cout << "Wpisz nazwe oraz cene produktu po spacji: ";
+        cin >> nazwa >> cena;
+        tab[i] = Produkt(nazwa, cena);
+    }
+    bubbleSort(tab, 10);
+    for (auto x : tab) {
+        cout << x.nazwa << " " << x.cena << endl;
+    }
+}
